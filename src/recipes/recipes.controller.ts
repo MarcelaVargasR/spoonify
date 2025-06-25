f0d14d1169e06c5699804dcbf269228287f49624
Marcela <lmvt.07@gmail.com>
2025-06-25 15:45:23 -0600

    feat: enhance recipes controller and router with CRUD operations

--- src/recipes/recipes.controller.ts
+++ src/recipes/recipes.controller.ts
@@ -2,14 +2,6 @@ import { Request, Response } from "express";
 import { RecipeModel } from "./Recipe.model";
 import { RecipeType } from "../types/recipe.type";
 
-async function getrecipes(_req: Request, res: Response) {
-  const recipes = await RecipeModel.find();
-
-  res.json({
-    recipes,
-  });
-}
-
 async function createRecipe(req: Request, res: Response) {
   const body: RecipeType = req.body;
 
@@ -23,4 +15,43 @@ async function createRecipe(req: Request, res: Response) {
   });
 }
 
-export { getrecipes, createRecipe };
+async function getrecipes(_req: Request, res: Response) {
+  const recipes = await RecipeModel.find();
+
+  res.json({
+    recipes,
+  });
+}
+
+async function getRecipeById(req: Request, res: Response) {
+  const recipe = await RecipeModel.findById(req.params.id);
+  res.json({
+    success: true,
+    data: recipe,
+  });
+}
+
+async function updateRecipeById(req: Request, res: Response) {
+  const body: RecipeType = req.body;
+  const recipeId = req.params.id;
+  const updateRecipe = await RecipeModel.findByIdAndUpdate(recipeId, body, {
+    new: true,
+  });
+
+  res.json(updateRecipe);
+}
+
+async function deleteRecipeById(req: Request, res: Response) {
+  const recipeId = req.params.id;
+  const deleteRecipe = await RecipeModel.findByIdAndDelete(recipeId);
+
+  res.json({ _id: deleteRecipe });
+}
+
+export {
+  createRecipe,
+  getrecipes,
+  getRecipeById,
+  updateRecipeById,
+  deleteRecipeById,
+};
--- src/recipes/recipes.router.ts
+++ src/recipes/recipes.router.ts
@@ -1,9 +1,18 @@
 import { Router } from "express";
-import { getrecipes, createRecipe } from "./recipes.controller";
+import {
+  getrecipes,
+  createRecipe,
+  updateRecipeById,
+  getRecipeById,
+  deleteRecipeById,
+} from "./recipes.controller";
 
 const router = Router();
 
 router.route("/").get(getrecipes);
 router.route("/").post(createRecipe);
+router.route("/:id").put(updateRecipeById);
+router.route("/:id").get(getRecipeById);
+router.route("/:id").delete(deleteRecipeById);
 
 export { router };
